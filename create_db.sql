f60bef0ac6b0e018b81c96e153abdf4c00ac4c1c
Saul Mestanza <saul.mm92@gmail.com>
2019-01-13 09:44:23 -0500

    Correciones

--- create_db.sql
+++ create_db.sql
@@ -1,5 +1,5 @@
 CREATE DATABASE `cbb_db`;
-CREATE USER 'cbb_user'@'localhost' IDENTIFIED BY 'cbb_password';
-GRANT ALL PRIVILEGES ON * . * TO 'cbb_user'@'localhost';
+CREATE USER 'cbb_user'@'localhost' IDENTIFIED BY 'g/u}5?n~sA2x<fR>*(5~es(v]P}MC<fg';
+GRANT ALL PRIVILEGES ON * . * TO 'cbb_user'@'%';
 FLUSH PRIVILEGES;
 USE `cbb_db`;
