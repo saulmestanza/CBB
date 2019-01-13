7b6670ba8f03adbbc217b0c550234f5dbf46f9f1
Saul Mestanza <saul.mm92@gmail.com>
2019-01-13 09:45:07 -0500

    ignore

--- create_db.sql
+++
@@ -1,5 +0,0 @@
-CREATE DATABASE `cbb_db`;
-CREATE USER 'cbb_user'@'localhost' IDENTIFIED BY 'g/u}5?n~sA2x<fR>*(5~es(v]P}MC<fg';
-GRANT ALL PRIVILEGES ON * . * TO 'cbb_user'@'%';
-FLUSH PRIVILEGES;
-USE `cbb_db`;
--- recreate_db.sql
+++
@@ -1,5 +0,0 @@
-DROP DATABASE cbb_db;
-CREATE DATABASE `cbb_db`;
-GRANT ALL PRIVILEGES ON * . * TO 'cbb_user'@'localhost';
-FLUSH PRIVILEGES;
-USE cbb_db;
